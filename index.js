523135e7e25f7ded276d24eaea841a9db4b6a34c
Pranav <pranavkanwar4@gmail.com>
2024-01-22 19:47:18 +0530

    Setup views and some routes and controllers

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-app.use('/', require('./routes'));
+app.use('/', require('./routes/index'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, (err) => {
     if(err){
