a1a588ccf8e0001d78fcf4fbe6b3a22db93e243e
Pranav <pranavkanwar4@gmail.com>
2024-01-23 19:44:57 +0530

    setup static files and mongodb connection

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const expressLayouts = require("express-ejs-layouts");
+const db = require('./config/mongoose');
 
+
+app.use(express.static('./assets'));
+app.use(expressLayouts);
 app.use('/', require('./routes/index'));
+
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
