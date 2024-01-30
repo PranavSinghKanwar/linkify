51de30d737d220c43517fc36beee82a78e66c4f0
Pranav <pranavkanwar4@gmail.com>
2024-01-30 11:10:02 +0530

    add delete functionality to posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/posts');
+const Comment = require('../models/comment');
 
 module.exports.create = async function(req, res){
     try{
@@ -11,5 +12,25 @@ module.exports.create = async function(req, res){
     catch(err){
         console.log('error in creating a post');
         return;
+    }
+}
+
+module.exports.destroy = async function(req, res){
+    try{
+        let delPost = await Post.findById(req.params.id);
+        
+        // .id means converting the object id into string
+        if(delPost.user == req.user.id){
+            let deleted = await Post.findByIdAndDelete(req.params.id);
+            let deletedComments = await Comment.deleteMany({post: req.params.id});
+            res.redirect('back');
+        }  
+        else{
+            return res.redirect('back');
+        }
+    }
+    catch(err){
+        console.log(err);
+        return res.redirect('back');
     }
 }
